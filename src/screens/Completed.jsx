027354a81a2aafa7e3d6b7eae2861c07aae3edde
JAMSHED ALI <alij0940@gmail.com>
2024-12-28 20:45:02 +0530

    Update

--- src/screens/Completed.jsx
+++ src/screens/Completed.jsx
@@ -1,14 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import TextinputComp from '../components/TextinputComp'
+import { moderateScaleVertical } from '../styles/responsiveSize'
+import ButtonComp from '../components/ButtonComp'
+
 
 const Completed = () => {
   return (
-    <View>
-      <Text>Completed</Text>
+    <View style={{...styles.container}}>
+     <TextinputComp placeholder={'Title'}/>
+     <TextinputComp placeholder={'Detail'}/>
+     <ButtonComp title={'ADD'}/>
     </View>
   )
 }
 
 export default Completed
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+      gap:moderateScaleVertical(15)
+    }
+})
